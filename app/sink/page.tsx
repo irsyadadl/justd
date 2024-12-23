e8a859a5f8c35ad2c1384a93c2694c35dc831329
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-12-23 16:19:45 +0700

    wip

--- app/sink/page.tsx
+++ app/sink/page.tsx
@@ -1,14 +1,7 @@
 "use client"
 
-import DateRangePickerDemo from "@/components/docs/date-and-time/date-range-picker/date-range-picker-demo"
-import { DateRangePicker } from "ui"
-
 export default function Page() {
   return (
-    <div className="grid gap-6 p-2 sm:grid-cols-3 sm:p-32">
-      <DateRangePickerDemo />
-      <DateRangePicker visibleDuration={{ months: 2 }} label="Event date" />
-      {/*<DatePickerDemo/>*/}
-    </div>
+      <div/>
   )
 }
