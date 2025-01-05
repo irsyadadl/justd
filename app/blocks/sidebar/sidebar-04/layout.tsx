8e94bb0b5e9f4bbbc6f5eadde792c2e0075092a6
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-01-05 22:09:42 +0700

    wip

--- app/blocks/sidebar/sidebar-04/layout.tsx
+++ app/blocks/sidebar/sidebar-04/layout.tsx
@@ -5,14 +5,12 @@ import AppSidebarNav from "../app-sidebar-nav"
 
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <>
-      <SidebarProvider>
-        <AppSidebar intent="float" />
-        <SidebarInset>
-          <AppSidebarNav />
-          <div className="p-4 lg:p-6">{children}</div>
-        </SidebarInset>
-      </SidebarProvider>
-    </>
+    <SidebarProvider>
+      <AppSidebar intent="float" />
+      <SidebarInset>
+        <AppSidebarNav />
+        <div className="p-4 lg:p-6">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
   )
 }
--- app/blocks/sidebar/sidebar-16/layout.tsx
+++ app/blocks/sidebar/sidebar-16/layout.tsx
@@ -5,14 +5,12 @@ import AppSidebarNav from "../app-sidebar-nav"
 
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <>
-      <SidebarProvider>
-        <SidebarInset>
-          <AppSidebarNav />
-          <div className="p-4 lg:p-6">{children}</div>
-        </SidebarInset>
-        <AppSidebar side="right" collapsible="dock" />
-      </SidebarProvider>
-    </>
+    <SidebarProvider>
+      <SidebarInset>
+        <AppSidebarNav />
+        <div className="p-4 lg:p-6">{children}</div>
+      </SidebarInset>
+      <AppSidebar side="right" collapsible="dock" />
+    </SidebarProvider>
   )
 }
