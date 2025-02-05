03617f3be975a44b67709aeef2f904655bc6781b
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-02-05 19:44:10 +0700

    add value label docs to meter

---
+++ components/docs/statuses/meter/meter-value-label-demo.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { Meter } from "ui"
+
+export default function MeterValueLabelDemo() {
+  return (
+    <Meter
+      formatOptions={{
+        style: "decimal",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      }}
+      valueLabel="25GB of 100GB"
+      label="Progress"
+      value={75.25}
+    />
+  )
+}
